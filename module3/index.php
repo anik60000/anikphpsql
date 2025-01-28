3539e86cb087d509f19ed32643c2b47ee733e6a3
anik60000 <aaanikuu@gmail.com>
2025-01-28 16:16:23 +0100

    module3

---
+++ module3/index.php
@@ -0,0 +1,50 @@
+<?php
+
+
+$score=63;
+
+if{$score>90}{
+    echo "Best Grade 5";
+}
+elseif($score>=80 && $score71){
+    echo "4"
+}
+elseif($score>=70 && $score59){
+    echo "3"
+}
+elseif(score>=60 && $score49){
+    echo "2"
+}
+elseif($score<50 && $score0){
+    echo "1"
+}
+
+
+
+
+
+
+
+$day ="MONDAY";
+
+switch($day){
+    case "monday":
+    echo "ski";
+    break;
+    case "tuesday":
+    echo "ki"
+    break;
+    case "wednesday":
+    echo "ski"
+    break;
+    case "thursday":
+    echo "ki"
+    break;
+    case "Friday":
+    echo "ski";
+    break;
+    default:
+    echo "Vikend";
+    break;
+}
+
