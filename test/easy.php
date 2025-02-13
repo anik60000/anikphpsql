3bc77c7f858860a3e9357be99d45a5650f88125c
anik60000 <aaanikuu@gmail.com>
2025-02-13 16:00:53 +0100

    easy

---
+++ test/easy.php
@@ -0,0 +1,32 @@
+<?php
+  
+  $students = array (
+    array (
+    "name"=> "Andi",
+    "age"=>15,
+    "veni"=>"Emshir"),
+
+
+    array (
+        "name"=> "Eden",
+        "age"=>16,
+        "veni"=>"qaty dikun"),
+
+
+        array (
+            "name"=> "Nil",
+            "age"=>15,
+            "veni"=>"FK"),
+
+
+        );
+
+
+
+
+
+
+
+
+
+?>
